251bbecc81ccade2200baafff6395d1bd5f9a0b9
samihaddad85 <haddad0@sheridancollege.ca>
2021-02-21 16:10:42 -0500

    Added author name Sami Haddad

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -14,6 +14,7 @@ import java.util.Collections;
  *
  * @author dancye
  * @author Paul Bonenfant Jan 2020
+ * @author Sami Haddad Jan 2020
  */
 public class GroupOfCards {
 
--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -10,6 +10,7 @@ package ca.sheridancollege.project;
  *
  * @author dancye
  * @author Paul Bonenfant Jan 2020
+ * @author Sami Haddad Jan 2020
  */
 public abstract class Player {
 
